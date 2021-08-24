91ce946624a793f69ea56bd5a3ba07a9b2e27166
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-24 15:40:04 -0400

    hm

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,6 @@
 import 'package:cardanosmartproj/model/colors.dart';
 import 'package:cardanosmartproj/model/qrbackcanada.dart';
+import 'package:cardanosmartproj/view/appbar.dart';
 import 'package:cardanosmartproj/view/title.dart';
 import 'package:flutter/material.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
@@ -12,14 +13,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  double screenw, screenh;
-
-  final Duration duration = Duration(milliseconds: 300);
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    screenh = size.height;
-    screenw = size.width;
     return Container(
       child: Padding(
         padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
@@ -34,30 +29,6 @@ class _HomeState extends State<Home> {
             SizedBox(height: 50),
             QrCode()
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget dashboard(context) {
-    return AnimatedPositioned(
-      top: 0.2 * screenh,
-      bottom: 0.2 * screenw,
-      left: 0.7 * screenw,
-      right: -0.5 * screenw,
-      duration: duration,
-      child: Material(
-        elevation: 8,
-        color: backdropper1,
-        child: Container(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-              )
-            ],
-          ),
         ),
       ),
     );
